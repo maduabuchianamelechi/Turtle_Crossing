75dd8c111daad889d1a7a6301c7975a11468d27a
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-09-15 16:03:40 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+gamer = Player()
+car_manager = CarManager()
+screen.listen()
+screen.onkey(key="Up", fun=gamer.move_up)
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with cars
+    for car in car_manager.cars:
+        if gamer.distance(car) < 20:
+            game_is_on = False
+
+screen.exitonclick()
+
+
